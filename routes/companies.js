8cd6c99ab0b017646a9c77de18c90edf1f08dac0
Austin E <austinjjelliott@gmail.com>
2024-10-29 21:26:18 -0700

    finished part 2

---
+++ routes/companies.js
@@ -0,0 +1,97 @@
+const db = require("../db");
+const express = require("express");
+const ExpressError = require("../expressError");
+const router = express.Router();
+
+/** (Fixed) Get users: [user, user, user] */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const results = await db.query(
+      `SELECT code, name, description FROM companies`
+    );
+    return res.json({ companies: results.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get("/:code", async function (req, res, next) {
+  try {
+    const userQuery = await db.query(
+      "SELECT code, name, description FROM companies WHERE code = $1",
+      [req.params.code]
+    );
+    if (userQuery.rows.length === 0) {
+      let notFoundError = new Error(
+        `There is no user with code of '${req.params.code}'`
+      );
+      notFoundError.status = 404;
+      throw notFoundError;
+    }
+    return res.json({ company: userQuery.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post("/", async function (req, res, next) {
+  try {
+    const result = await db.query(
+      `INSERT INTO companies (code, name, description) 
+            VALUES ($1, $2, $3)
+            RETURNING code, name, description`,
+      [req.body.code, req.body.name, req.body.description]
+    );
+
+    return res.status(201).json({ company: result.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.put("/:code", async function (req, res, next) {
+  try {
+    if ("code" in req.body) {
+      throw new ExpressError("Not Allowed", 400);
+    }
+    const result = await db.query(
+      `
+        UPDATE companies
+        SET name = $1, description = $2
+        WHERE code = $3
+        RETURNING code, name, description`,
+      [req.body.name, req.body.description, req.params.code]
+    );
+    if (result.rows.length === 0) {
+      throw new ExpressError(
+        `There is no company with code of ${req.params.code}`,
+        404
+      );
+    }
+    return res.json({ company: result.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.delete("/:code", async function (req, res, next) {
+  try {
+    const result = await db.query(
+      `DELETE FROM companies WHERE code = $1 RETURNING code`,
+      [req.params.code]
+    );
+    if (result.rows.length === 0) {
+      throw new ExpressError(
+        `There is no company with code of "${req.params.code}"`,
+        404
+      );
+    }
+    return res.json({ status: "deleted" });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// HAVE TO EXPORT ROUTER FOR APP TO USE IT!
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 /** Server startup for BizTime. */
 
-
 const app = require("./app");
 
-
 app.listen(3000, function () {
-  console.log("Listening on 3000");
+  console.log("Server starting on port 3000");
 });
